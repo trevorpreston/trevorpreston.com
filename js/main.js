ded98ae182cfeb024c9615375a788d41d57d58b3
Trevor Preston <trevorpreston4@gmail.com>
2016-08-28 13:42:41 -0400

    fixed broken anchor tags

--- js/main.js
+++ js/main.js
@@ -171,66 +171,6 @@ var controller2 = new ScrollMagic.Controller({
   }
 });
 
-var scenes = {
-  'scene1': {
-    'section-1': 'anchor1'
-  },
-  'scene2': {
-    'section-2': 'anchor2'
-  },
-  'scene3': {
-    'section-3': 'anchor3'
-  }
-}
-
-for(var key in scenes) {
-  // skip loop if the property is from prototype
-  if (!scenes.hasOwnProperty(key)) continue;
-
-  var obj = scenes[key];
-
-  for (var prop in obj) {
-    // skip loop if the property is from prototype
-    if(!obj.hasOwnProperty(prop)) continue;
-
-    new ScrollMagic.Scene({ triggerElement: '#' + prop })
-        .setClassToggle('#' + obj[prop], 'active')
-        .addTo(controller2);
-  }
-}
-
-// Change behaviour of controller
-// to animate scroll instead of jump
-controller2.scrollTo(function(target) {
-
-  TweenMax.to(window, 0.5, {
-    scrollTo : {
-      y : target,
-      autoKill : true // Allow scroll position to change outside itself
-    },
-    ease : Cubic.easeInOut
-  });
-
-});
-
-var anchor_nav = document.querySelector('.anchor-nav');
-
-anchor_nav.addEventListener('click', function(e) {
-  var target = e.target,
-      id     = target.getAttribute('href');
-
-  if(id !== null) {
-    if(id.length > 0) {
-      e.preventDefault();
-      controller.scrollTo(id);
-
-      if(window.history && window.history.pushState) {
-        history.pushState("", document.title, id);
-      }
-    }
-  }
-});
-
 // build scenes
 
   new ScrollMagic.Scene({triggerElement: ".about"})
@@ -256,6 +196,3 @@ anchor_nav.addEventListener('click', function(e) {
 
   cycleScroller(scroller);
 });
-
-
-
